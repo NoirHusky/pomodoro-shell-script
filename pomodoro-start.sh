85272004cbff6437b309eafa4f465983971494c5
NoirHusky <samuelverev@gmail.com>
2022-02-04 14:48:10 +0100

    added count of pomodoros

--- pomodoro-start.sh
+++ pomodoro-start.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
+
+count=0;
 while [ 1 ]
 do
-	xmessage "Time to work"
+    xmessage "Time to work"
     secs=1500
     while [ $secs -gt -1 ]
     do
-		clear
+	clear
     	sleep 1 &
-    	printf "Pomodoro (#Work): \t%02d:%02d:%02d" $((secs/3600)) $(( (secs/60)%60)) $((secs%60))
+	printf "Pomodoro (#Work): \t%02d:%02d:%02d\nPomodoros (#%d)" $((secs/3600)) $(( (secs/60)%60)) $((secs%60)) $(( count ))
         secs=$(( $secs - 1 ))
     	wait
     done
+    count=$(( $count + 1 ))
     
     xmessage "Time to take a break"    
     secs=300
     while [ $secs -gt -1 ]
     do
-		clear
+	clear
     	sleep 1 &
-    	printf "Pomodoro (#Break): \t%02d:%02d:%02d" $((secs/3600)) $(( (secs/60)%60)) $((secs%60))
+	printf "Pomodoro (#Break): \t%02d:%02d:%02d\nPomodoros (#%d)" $((secs/3600)) $(( (secs/60)%60)) $((secs%60)) $(( count ))
         secs=$(( $secs - 1 ))
     	wait
     done
